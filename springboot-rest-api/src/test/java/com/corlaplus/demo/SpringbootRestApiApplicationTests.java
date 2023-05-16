a4270db39c5a33d8f8a0ef9cc285ec208ef0f79b
Leslie Clottey <tcclotteygh@gmail.com>
2023-05-16 12:03:12 -0400

    Initial Commit

---
+++ springboot-rest-api/src/test/java/com/corlaplus/demo/SpringbootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corlaplus.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
